f96a019f6697bd059b0986238157ab899c7fb725
varunmehta14 <mehtavarunj@gmail.com>
2021-01-26 22:19:12 +0530

    Updates

---
+++ precache-manifest.f20e2b5b40a352875bcb5b9720422a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd55a7028a8fbf549fe331e02a07d9a7",
+    "url": "/video-browser/index.html"
+  },
+  {
+    "revision": "d0f34f4e94dc6ae37690",
+    "url": "/video-browser/static/css/main.7a838314.chunk.css"
+  },
+  {
+    "revision": "fe0332b29aa3c6a91945",
+    "url": "/video-browser/static/js/2.18861583.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/video-browser/static/js/2.18861583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f34f4e94dc6ae37690",
+    "url": "/video-browser/static/js/main.1a7da904.chunk.js"
+  },
+  {
+    "revision": "a76e1724ee874ce35160",
+    "url": "/video-browser/static/js/runtime-main.5b4eadd6.js"
+  }
+]);
